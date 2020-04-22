2c52c9b4a92912d1fde7cf2775bd370ab36fc7cc
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-04-22 18:58:55 -0400

    BigQuery, refactor findFile to utils

---
+++ bigQuery/bigQuery.py
@@ -0,0 +1,30 @@
+from google.cloud import bigquery
+import os
+from utils.util import findFile
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = findFile("bigquery.json")
+
+
+class BigQ:
+
+    def __init__(self):
+        self.client = bigquery.Client()
+
+    def getTime(self):
+        query = "SELECT CURRENT_DATETIME('America/New_York') as now;"
+        query_job = self.client.query(query)
+        for row in query_job:
+            # Row values can be accessed by field name or index.
+            return row["now"]
+
+    def select(self, query):
+        query_job = self.client.query(query)
+        return query_job
+
+        """ % (id, ret, msg, self.getTime(), raw, label)
+
+      query_job = self.client.query(query)
+      [x for x in query_job]  # Make an API request.
+      print("result:", query_job)
+      return query_job
+      """
---
+++ test/test_big_query.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from bigQuery.bigQuery import BigQ
+
+
+class BigQTestSuite(TestCase):
+
+    def test_time(self):
+        bigQ = BigQ()
+        print(bigQ.getTime())
