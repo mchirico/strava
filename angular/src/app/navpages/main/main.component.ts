31cd8394c4283ee46dfae179ecc827359f027c54
Mike Chirico <mchirico@gmail.com>
2020-05-01 19:52:26 -0400

    Angularfire updates (#14)

--- angular/src/app/navpages/main/main.component.ts
+++ angular/src/app/navpages/main/main.component.ts
@@ -1,9 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {CookieService} from 'ngx-cookie-service';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
+import {AngularFireAuth} from "@angular/fire/auth";
 
+export interface Item {
+  name: string;
+  food?: string;
+}
 
 function buildURL(code: string) {
   const url = "https://www.strava.com/oauth/authorize?client_id=7704&state=" + code + "&redirect_uri=" +
@@ -21,19 +26,26 @@ function buildURL(code: string) {
 })
 export class MainComponent implements OnInit {
   items: Observable<any[]>;
-
+  item: Observable<Item>;
   url = '';
   state = '';
   access_token = '';
   refresh_token = '';
   lastname = '';
   firstname = '';
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  private itemDoc: AngularFirestoreDocument<Item>;
 
   constructor(private route: ActivatedRoute, private cookieService: CookieService,
-              private firestore: AngularFirestore) {
+              private afs: AngularFirestore, public auth: AngularFireAuth) {
 
+    this.itemsCollection = afs.collection<Item>('items');
 
-    this.items = firestore.collection('items').valueChanges();
+    this.items = this.itemsCollection.valueChanges();
+
+    this.itemDoc = afs.doc<Item>('items/1');
+    this.item = this.itemDoc.valueChanges();
+    // this.items = afs.collection('items').valueChanges();
 
     this.cookieService.set('Test', 'Hello World');
     this.state = this.cookieService.get('state');
@@ -53,10 +65,17 @@ export class MainComponent implements OnInit {
     });
 
 
-
   }
 
   ngOnInit(): void {
+  }
+
+  addItem(item: Item) {
+    this.itemsCollection.add(item);
+  }
+
+  update(item: Item) {
+    this.itemDoc.update(item);
   }
 
 }
